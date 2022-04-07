d7ea469f827e9ea58338034786d45a693d58d53c
Vo Thanh Cang <vtcanglt@gmail.com>
2022-04-07 14:45:26 +0700

    Added : WO schedma

--- src/Schemas/WO.php
+++ src/Schemas/WO.php
@@ -6,4 +6,8 @@ namespace MakiniAdapter\MongoConverter\Schemas;
 class WO extends PM
 {
     const DUE_AT = 'dueAt';
+    const SCHEDULED_AT = 'scheduledAt';
+    const COMPLETED_AT = 'completedAt';
+    const ASSIGNED_USER_KEY = 'assignedUserKey';
+    const COMPLETED_USER_KEY = 'completedUserKey';
 }
