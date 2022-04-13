acdc8d15d391aedade969f9d532da1dd1fe04344
Vo Thanh Cang <vtcanglt@gmail.com>
2022-04-13 11:41:20 +0700

    Added : counter

---
+++ src/Schemas/Counter.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace MakiniAdapter\MongoConverter\Schemas;
+
+
+class Counter extends Schema
+{
+    const LAST_READING_AT = 'lastReadingAt';
+    const LAST_READING = 'lastReading';
+}
