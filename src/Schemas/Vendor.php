fa560b393a882901bf8a935a44febaaeddb6b67f
Vo Thanh Cang <vtcanglt@gmail.com>
2022-04-13 17:41:27 +0700

    Added : vendor

---
+++ src/Schemas/Vendor.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace MakiniAdapter\MongoConverter\Schemas;
+
+
+class Vendor extends Schema
+{
+    const COUNTRY_NAME = 'countryName';
+    const CURRENCY = 'currency';
+    const ADDRESS_LINES = 'addressLines';
+    const CITY = 'city';
+    const POST_CODE = 'postCode';
+    const PHONE = 'phone';
+    const EMAIL = 'email';
+}
