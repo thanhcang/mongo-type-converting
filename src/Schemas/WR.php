5b0bced62f55cefb09f3a90f0baec927ac2e0748
Vo Thanh Cang <vtcanglt@gmail.com>
2022-04-15 11:50:52 +0700

    Added : WR

---
+++ src/Schemas/WR.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace MakiniAdapter\MongoConverter\Schemas;
+
+class WR extends PM
+{
+    const USER_REQUESTED_KEY = 'userRequestedKey';
+    const USER_APPROVED_KEY = 'userApprovedKey';
+    const WORK_ORDER_KEY = 'workOrderKey';
+}
