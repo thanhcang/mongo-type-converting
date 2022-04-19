458a5b622fce05573a2e4560a23d6168fc59eda9
Vo Thanh Cang <vtcanglt@gmail.com>
2022-04-19 10:30:58 +0700

    Added : calculation

---
+++ src/Calculation.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace MakiniAdapter\MongoConverter;
+
+
+use JetBrains\PhpStorm\ArrayShape;
+
+trait Calculation
+{
+    #[ArrayShape(['$multiply' => "array"])]
+    public static function multiply(
+        array $expression
+    ): array {
+        return ['$multiply' => $expression];
+    }
+}
