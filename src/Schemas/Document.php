c5fff34188a10fa6389f9f452e6bf954ab60ec90
Vo Thanh Cang <vtcanglt@gmail.com>
2022-10-04 20:34:21 +0700

    Added : document keys

--- src/Schemas/Document.php
+++ src/Schemas/Document.php
@@ -9,5 +9,6 @@ class Document extends Schema
     const ENTITY_KEYS = 'entityKeys';
     const URL = 'url';
     const CONTENT_TYPE = 'contentType';
-
+    const UPLOAD = 'upload';
+    const MINE_TYPE = 'mimeType';
 }
